34b0955022535f1b0054257eecc347b48169061b
Aditya L <112402287+laditya2805@users.noreply.github.com>
2025-10-31 18:37:01 +0530

    Refactor import path for LambdaInfraStack

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { LambdaInfraStack } from "../lib/lambda-infra-stack";
+import { LambdaInfraStack } from "../lib/lambda-stack";
 const app = new cdk.App();
 const envParam = app.node.tryGetContext("env") || "dev";
 new LambdaInfraStack(app, `EmeraldOnPremStack-${envParam}`, {
  env: { region: "ap-south-1" },
  environment: envParam,
 });
+
