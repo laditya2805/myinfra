3f5678d7ba6475463011740d597d10c8b93e12d2
Aditya Lingineni <laditya304@gmail.com>
2025-11-01 16:01:56 +0530

    eslinfix

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,4 +1,6 @@
 const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
 
 module.exports = [
   js.configs.recommended,
@@ -6,13 +8,23 @@ module.exports = [
     files: ["**/*.ts"],
     ignores: ["dist/**", "node_modules/**"],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
+        project: "./tsconfig.json",
       },
     },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
     rules: {
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^(event|context)$" }],
+      ...tsPlugin.configs.recommended.rules, // add recommended TS rules
+      "no-unused-vars": "off", // disable base rule
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^(event|context)$" },
+      ],
       "no-console": "off",
     },
   },
