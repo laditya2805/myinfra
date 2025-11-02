e169fa9f7962d54ab6aaf71054815f94685e41b0
Aditya Lingineni <laditya304@gmail.com>
2025-11-02 18:27:51 +0530

    cdkcontextfix

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -2,7 +2,7 @@
 import * as cdk from "aws-cdk-lib";
 import { LambdaInfraStack } from "../lib/lambda-stack";
 const app = new cdk.App();
-const envParam = app.node.tryGetContext("env") || "dev";
+const envParam = app.node.tryGetContext("environment") || "dev";
 new LambdaInfraStack(app, `EmeraldOnPremStack-${envParam}`, {
  env: { region: "ap-south-1" },
  environment: envParam,
